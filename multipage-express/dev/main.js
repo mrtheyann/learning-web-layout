7481b8b254e3936e28b6a71895d1976fedf43892
mrtheyann <darklodr@gmail.com>
2017-05-15 23:38:20 +0500

    upd: 36.07%

--- multipage-express/dev/main.js
+++ multipage-express/dev/main.js
@@ -1,22 +1,47 @@
 var express = require('express');
   app = express();
 
-app.disable('x-powered-by');
+var store = {
+  home: {
+    page: 'Our Super Awesome App',
+    content: 'Home, sweet home'
+  },
+  about: {
+    page: 'About',
+    content: 'It\'s an express framework project'
+  },
+  downloads: {
+    page: 'Downloads Page',
+    content: 'Download all stuff here'
+  },
+  profile: {
+    page: 'Profile Page',
+    content: 'This is your profile, dawg'
+  }
+};
 
-app.use(function(req, res, next){
+app.disable('x-powered-by');
+app.set('view engine', 'jade');
+app.use(function(req, res, next) {
   console.log('%s %s', req.method, req.url);
   next();
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.send('\
-    <h1>My page</h1>\
-    This is <mark>html</mark>\
-  ');
-})
+app.get('/:page?', function(req, res) {
+  var page = req.params.page, data;
+  console.log(page);
+  if (!page) page = 'home';
+  data = store[page];
+  if (!data) {
+    res.redirect('/');
+    return;
+  }
+  data.links = Object.keys(store);
+  res.render('main.jade', data);
+});
 
-server = app.listen(3000, function(){
+server = app.listen(3000, function() {
   console.log("Listening on port 3000");
-})
+});
