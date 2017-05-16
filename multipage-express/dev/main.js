767ea473533dcfd663dfcdc2037799ed7ee9d413
mrtheyann <darklodr@gmail.com>
2017-05-16 22:31:57 +0500

    upd: POST request

--- multipage-express/dev/main.js
+++ multipage-express/dev/main.js
@@ -1,5 +1,7 @@
-var express = require('express');
-  app = express();
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  server;
 
 var store = {
   home: {
@@ -20,25 +22,56 @@ var store = {
   }
 };
 
+var storeKeys = Object.keys(store);
+
 app.disable('x-powered-by');
 app.set('view engine', 'jade');
+
 app.use(function(req, res, next) {
   console.log('%s %s', req.method, req.url);
   next();
 });
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/about', function(req, res){
+  res.render('about', {
+    links: storeKeys
+  });
+});
+
+app.route('/new')
+  .get(function(req, res) {
+  res.render('new', {
+    page: 'Add New',
+    links: storeKeys
+    })
+  })
+  .post(function(req, res){
+    var data = req.body;
+    if (data.pageurl && data.pagename && data.pagecontent) {
+      store[data.pageurl] = {
+        page: data.pagename,
+        content: data.pagecontent
+      };
+      storeKeys = Object.keys(store);
+    }
+    res.redirect('/');
+  });
+
+
 
 app.get('/:page?', function(req, res) {
   var page = req.params.page, data;
-  console.log(page);
   if (!page) page = 'home';
   data = store[page];
   if (!data) {
     res.redirect('/');
     return;
   }
-  data.links = Object.keys(store);
+  data.links = storeKeys;
+  data.query = req.query;
   res.render('main.jade', data);
 });
 
