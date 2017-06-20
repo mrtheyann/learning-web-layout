62bb0d693d22d13a580182d71d4a45c99181053b
mrtheyann <darklodr@gmail.com>
2017-06-20 22:35:37 +0500

    add: representation

--- coffeescript-based/coffee-snap.svg/current/dist/js/main.js
+++ coffeescript-based/coffee-snap.svg/current/dist/js/main.js
@@ -1,65 +1,112 @@
 (function() {
-  var paper, path, pathArray, style, updatePath;
+  var GUI, RadialNav, describeArc, gui, polarToCartesian;
 
-  paper = Snap(800, 400);
-
-  console.log(paper);
-
-  style = {
-    fill: '#387',
-    stroke: '#222',
-    strokeWidth: 5
+  polarToCartesian = function(cx, cy, r, angle) {
+    angle = (angle - 90) * Math.PI / 180;
+    return {
+      x: cx + r * Math.cos(angle),
+      y: cy + r * Math.sin(angle)
+    };
   };
 
-  path = paper.path("").attr({
-    stroke: '#222',
+  describeArc = function(x, y, r, startAngle, endAngle) {
+    var end, large, start;
+    start = polarToCartesian(x, y, r, startAngle %= 360);
+    end = polarToCartesian(x, y, r, endAngle %= 360);
+    large = Math.abs(endAngle - startAngle) >= 180;
+    return "M" + start.x + "," + start.y + " A" + r + "," + r + ", 0 " + (large ? 1 : 0) + ", 1, " + end.x + ", " + end.y;
+  };
+
+  GUI = (function() {
+    function GUI(buttons) {
+      this.paper = Snap(window.innerWidth, window.innerHeight);
+      this.nav = new RadialNav(this.paper, buttons);
+      this._bindEvents();
+    }
+
+    GUI.prototype._bindEvents = function() {
+      return window.addEventListener('resize', (function(_this) {
+        return function() {
+          return _this.paper.attr({
+            width: window.innerWidth,
+            height: window.innerHeight
+          });
+        };
+      })(this));
+    };
+
+    return GUI;
+
+  })();
+
+  RadialNav = (function() {
+    function RadialNav(paper, buttons) {
+      this.area = paper.svg(0, 0, this.size = 500, this.size).addClass('radialnav');
+      this.c = this.size / 2;
+      this.r = this.size * .25;
+      this.r2 = this.r * .35;
+      this.angle = 360 / buttons.length;
+      this.container = this.area.g();
+      this.updateButtons(buttons);
+    }
+
+    RadialNav.prototype._sector = function() {
+      return this.area.path.addClass('radialnav-sector');
+    };
+
+    RadialNav.prototype.updateButtons = function(buttons) {
+      return this.container.clear();
+    };
+
+    return RadialNav;
+
+  })();
+
+  gui = new GUI([
+    {
+      icon: 'pin',
+      action: function() {
+        return console.log('Pinning...');
+      }
+    }, {
+      icon: 'search',
+      action: function() {
+        return console.log('Opening Search...');
+      }
+    }, {
+      icon: 'cloud',
+      action: function() {
+        return console.log('Connecting to Cloud...');
+      }
+    }, {
+      icon: 'settings',
+      action: function() {
+        return console.log('Opening Settings...');
+      }
+    }, {
+      icon: 'rewind',
+      action: function() {
+        return console.log('Rewinding...');
+      }
+    }, {
+      icon: 'preview',
+      action: function() {
+        return console.log('Preview Activated...');
+      }
+    }, {
+      icon: 'delete',
+      action: function() {
+        return console.log('Deleting...');
+      }
+    }
+  ]);
+
+  gui.paper.path(describeArc(200, 200, 120, 0, 45)).attr({
     fill: 'transparent',
-    strokeWidth: 3
+    stroke: '#fff',
+    strokeWidth: 4
   });
 
-  pathArray = [];
-
-  updatePath = function() {
-    var first, i, len, node, pathString, ref;
-    first = pathArray[0];
-    pathString = "M " + first.x + "," + first.y;
-    ref = pathArray.slice(1);
-    for (i = 0, len = ref.length; i < len; i++) {
-      node = ref[i];
-      pathString += "L " + node.x + "," + node.y;
-    }
-    return path.attr({
-      "d": pathString
-    });
-  };
-
-  paper.click(function(e) {
-    if (e.target.tagName === 'svg') {
-      paper.circle(e.offsetX, e.offsetY, 15).attr(style).data('i', pathArray.length).mouseover(function() {
-        return this.stop().animate({
-          r: 25
-        }, 1000, mina.elastic);
-      }).mouseout(function() {
-        return this.stop().animate({
-          r: 15
-        }, 300, mina.elasticinout);
-      }).drag(function(dx, dy, x, y) {
-        var currentNode;
-        this.attr({
-          cx: x,
-          cy: y
-        });
-        currentNode = pathArray[this.data('i')];
-        currentNode.x = x;
-        currentNode.y = y;
-        return updatePath();
-      });
-      pathArray.push({
-        x: e.offsetX,
-        y: e.offsetY
-      });
-      return updatePath();
-    }
-  });
+  console.log(gui.paper);
 
 }).call(this);
